583c378bf89c731179f4121757af7f7d328457b1
shruti panwar <Shrutipanwar312@gmail.com>
2025-11-14 18:34:17 +0530

    add

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,10 +10,10 @@ const deliveryCharge = 10
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-const razorpayInstance = new razorpay({
-    key_id : process.env.RAZORPAY_KEY_ID,
-    key_secret : process.env.RAZORPAY_KEY_SECRET,
-})
+// const razorpayInstance = new razorpay({
+//     key_id : process.env.RAZORPAY_KEY_ID,
+//     key_secret : process.env.RAZORPAY_KEY_SECRET,
+// })
 
 // Placing orders using COD Method
 const placeOrder = async (req,res) => {
